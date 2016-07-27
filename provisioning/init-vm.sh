8e0c1e24a87cfa1e84bbba4e475ff8964b28d5a7
Tim Bezhashvyly <tim.bezhashvyly@gmail.com>
2016-07-27 10:56:22 +0200

    Issue #1: Remove composer SHA verification

--- provisioning/init-vm.sh
+++ provisioning/init-vm.sh
@@ -71,7 +71,6 @@ bash < <(wget -q --no-check-certificate -O - https://raw.github.com/colinmollenh
 sudo mv ~/bin/modman /usr/local/bin/modman
 
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php -r "if (hash_file('SHA384', 'composer-setup.php') === '92102166af5abdb03f49ce52a40591073a7b859a86e8ff13338cf7db58a19f7844fbc0bb79b2773bf30791e935dbd938') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 php composer-setup.php
 php -r "unlink('composer-setup.php');"
 sudo mv composer.phar /usr/local/bin/composer
