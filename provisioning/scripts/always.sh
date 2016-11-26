c2da8f45ffb942875cbf4b585af08a733d408408
Vinai Kopp <vinai@netzarbeiter.com>
2016-11-26 11:02:03 +0100

    Issue #23: Add note that initial import needs a little time

--- provisioning/scripts/always.sh
+++ provisioning/scripts/always.sh
@@ -10,5 +10,6 @@ source /vagrant/provisioning/sample/env
 /etc/init.d/lizards-and-pumpkins-consumers start
 
 echo -e "\n\nThe Developer VM is ready."
+echo "If this is the first boot, please give it a few minutes to complete the initial import."
 echo "If you haven't done so yet, please add '192.168.56.121 demo.lizardsandpumpkins.com.loc' to your local hosts file."
 echo "After that you will be able to access Lizards & Pumpkins at http://demo.lizardsandpumpkins.com.loc/"
