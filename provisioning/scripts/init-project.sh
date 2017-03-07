afa25defc3028d362dbdfb959439592b93f9b53e
Vinai Kopp <vinai@netzarbeiter.com>
2017-03-07 07:42:51 +0100

    Issue #63: Switch to sample-project release 2017-03-07-02

--- provisioning/scripts/init-project.sh
+++ provisioning/scripts/init-project.sh
@@ -13,7 +13,7 @@ export LC_CTYPE=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
 
 [ ! -e sample-project ] && {
-    SAMPLE_PROJECT_VERSION=2017-02-14-01
+    SAMPLE_PROJECT_VERSION=2017-03-07-02
 
     echo "Cloning https://github.com/lizards-and-pumpkins/sample-project.git"
     git clone --quiet https://github.com/lizards-and-pumpkins/sample-project.git
